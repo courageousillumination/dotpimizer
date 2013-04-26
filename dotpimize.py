609945b5d6af2127aa14a5d955496aa584d29137
Stephen Rosen <sirosen@uchicago.edu>
2013-04-26 18:27:25 -0500

    Added stub main.

---
+++ dotpimize.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import pydot, sys
+
+if len(sys.argv) < 2:
+    print('Usage Error: dotpimizer.py regex-as-str',file=sys.stderr)
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    print('WOAH! THIS IS A STUB! WOAH! WOAH!'file=sys.stdout)
