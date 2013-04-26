70e5a05617a894fb4cc0530fb520ccb6682f8b9c
Tristan Rasmussen <tristanr93@gmail.com>
2013-04-26 18:48:23 -0500

    Added support for basic strings

--- regexgraph.py
+++ regexgraph.py
@@ -1,4 +1,17 @@
 import pydot
 
-def regex_to_graph(regex):
-    return ""
+def regex_to_graph(regex, graph_name = "regex_graph"):
+    """
+    Convert a regular expression to a pydot
+    graph object
+    """
+    graph = pydot.Dot(graph_name, graph_type='digraph')
+    graph.add_node(pydot.Node('0'))
+    state = 0
+    
+    #String code
+    for char in regex:
+        graph.add_node(pydot.Node(str(state + 1)))
+        graph.add_edge(pydot.Edge(str(state), str(state + 1), label=char))
+        state += 1
+    return graph
